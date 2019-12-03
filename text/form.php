38b26f8553bcff09ff88c42d917c56aea0526b5f
karl.ojarand <karl.ojarand@khk.ee>
2019-12-03 13:01:11 +0200

    Tekstifunktsioonid (ül9)

---
+++ text/form.php
@@ -0,0 +1,6 @@
+<?php
+?>
+<form action="ylesanne9.php" method="get">
+    Sisesta oma nimi <input type="text" name="nimi">
+    <input type="submit" value="Saada!">
+</form>
---
+++ text/ylesanne9.php
@@ -0,0 +1,10 @@
+<?php
+
+$nimi = $_GET ['nimi'];
+$nimi = strtolower($nimi);
+$nimi = ucfirst($nimi);
+echo 'Tere,'.' ' .$nimi. '!';
+
+echo '<hr>';
+
+
